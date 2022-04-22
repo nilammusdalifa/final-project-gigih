32759fc612495444cc450bb36ed8543f4dfb534d
nilammusdalifa <nilammusdalifa09@gmail.com>
2022-04-22 18:08:01 +0000

    Generate controller and view for order

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  resources :customers
+  resources :orders
   get 'order/index'
   get 'order/show'
   get 'order/new'
   get 'order/edit'
   resources :menus
+  resources :orders
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
