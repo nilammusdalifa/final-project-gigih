ee324ef76dbfaef119b427f0584ba39ef439c372
nilammusdalifa <nilammusdalifa09@gmail.com>
2022-04-22 16:28:12 +0000

    Create validation for model menu

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,5 @@
 class Customer < ApplicationRecord
   has_many :orders
+
+  # validate
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,8 @@
 class Menu < ApplicationRecord
   has_many :menu_categories
   has_many :categories, through: :menu_categories
+  
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :name, uniqueness: true
+  validates :description, length: { maximum: 150 }
 end
